7485ada1f4b9e4798e10f33ea86344e7f3ad6849
https.oksigen <david@yetim.me>
2024-06-05 21:19:12 +0400

    modified:   index.js

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('Salam'))
+bot.start((ctx) => {
 
-bot.on("message", (ctx) => ctx.reply(ctx.message.text))
+
+    ctx.reply('Salam')
+})
+
+bot.on("message", (ctx) => {
+    ctx.reply(ctx.message.text)
+    console.log(ctx.message.from.first_name);
+
+})
 
 bot.launch()
